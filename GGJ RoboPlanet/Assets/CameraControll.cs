87a2da24510156f15f0d3690fa09d23382621118
ChaosTheLegend <chaosthelegend@inbox.ru>
2020-02-02 03:22:01 +0600

    added whole project

---
+++ GGJ RoboPlanet/Assets/CameraControll.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControll : MonoBehaviour
+{
+    public GameObject Background;
+    public GameObject BackgroundPrefab;
+    public float BackgroundScale;
+
+    int bgsize = 1;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    void DoubleBG()
+    {
+        //bgBounds.size *= 3f;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Background.transform.position = transform.position * BackgroundScale;
+        Background.transform.position = new Vector3(Background.transform.position.x, Background.transform.position.y, 20f);
+
+        //camera.transform.rotation = transform.rotation;
+        //camera.transform.position = new Vector3(transform.position.x, transform.position.y, -1f) + CameraOffset;
+    }
+}
---
+++ GGJ RoboPlanet/Assets/PlanetRotation.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetRotation : MonoBehaviour
+{
+    public float RotationSpeed;
+    Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.angularVelocity = new Vector3(0f,RotationSpeed);
+    }
+}
